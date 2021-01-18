70f55de18c904993d1898c178b587b062e15813a
Samuel Cordeiro Silva Oliveira <40183730+SamuelCordeiro@users.noreply.github.com>
2021-01-18 00:04:15 -0300

    Player script creation.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private float jumpForce;
+    private bool isJumping;
+    public bool isAtk;
+    public bool isVisible;
+    public float visibleTime;
+    public GameObject weapon;
+    public GameObject weaponPoint;
+    private float visibleCount;
+    private float timeAtk;
+    private Rigidbody2D playerRigidbody;
+    private Animator playerAnimator;
+    private SpriteRenderer playerSpriteRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody2D>();
+        playerAnimator = GetComponent<Animator>();
+        playerSpriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        movement();
+        jump();
+        atk();
+        timeAtk -= Time.deltaTime;
+
+        if(isVisible)
+        {
+            visibleCount += Time.deltaTime;
+            if(visibleCount >= visibleTime)
+            {
+                visibleCount = 0;
+                isVisible = false;
+            }
+        }
+    }
+
+    private void movement()
+    {
+        if(Input.GetKey(KeyCode.D))
+        {
+            playerRigidbody.velocity = new Vector2(speed * Time.deltaTime, playerRigidbody.velocity.y);
+            playerAnimator.SetBool("isWalking", true);
+            transform.localEulerAngles = new Vector3(0,0,0);
+        }
+        else
+        {
+            playerRigidbody.velocity = new Vector2(0f, playerRigidbody.velocity.y);
+            playerAnimator.SetBool("isWalking", false);
+        }
+        if(Input.GetKey(KeyCode.A))
+        {
+            playerRigidbody.velocity = new Vector2(-speed * Time.deltaTime, playerRigidbody.velocity.y);
+            playerAnimator.SetBool("isWalking", true);
+            transform.localEulerAngles = new Vector3(0,180,0);
+        }
+    }
+
+    private void jump()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && !isJumping)
+        {
+            playerRigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            isJumping = true;
+            playerAnimator.SetBool("isJumping", true);
+        }
+    }
+
+    private void atk()
+    {
+        if(Input.GetKeyDown(KeyCode.E) && !isAtk)
+        {
+            playerAnimator.SetBool("isAtk", true);
+            isAtk = true;
+            timeAtk = 0.25f;
+        }
+        else if(timeAtk <= 0f)
+        {
+            playerAnimator.SetBool("isAtk", false);
+            isAtk = false;
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision) 
+    {
+        if(collision.gameObject.layer == 8)
+        {
+            isJumping = false;
+            playerAnimator.SetBool("isJumping", false);
+        } 
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider) 
+    {
+        if(collider.gameObject.tag == "GameOver")
+        {
+            GameController.current.gameOverPanel.SetActive(true);
+            Destroy(gameObject);
+        }
+    }
+
+    public IEnumerator PlayerDamage(float damageTime)
+    {
+        playerSpriteRenderer.enabled = false;
+        weapon.GetComponent<SpriteRenderer>().enabled = false;
+        yield return new WaitForSeconds(damageTime);
+        playerSpriteRenderer.enabled = true;
+        weapon.GetComponent<SpriteRenderer>().enabled = true;
+        yield return new WaitForSeconds(damageTime);
+        playerSpriteRenderer.enabled = false;
+        weapon.GetComponent<SpriteRenderer>().enabled = false;
+        yield return new WaitForSeconds(damageTime);
+        playerSpriteRenderer.enabled = true;
+        weapon.GetComponent<SpriteRenderer>().enabled = true;
+        yield return new WaitForSeconds(damageTime);
+        playerSpriteRenderer.enabled = false;
+        weapon.GetComponent<SpriteRenderer>().enabled = false;
+        yield return new WaitForSeconds(damageTime);
+        playerSpriteRenderer.enabled = true;
+        weapon.GetComponent<SpriteRenderer>().enabled = true;
+    }
+}
