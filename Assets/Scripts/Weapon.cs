33b83deb0fa592808d0a5a53869bc2669cd5c7bb
Samuel Cordeiro Silva Oliveira <40183730+SamuelCordeiro@users.noreply.github.com>
2021-01-22 01:37:18 -0300

    Weapon script creation.

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject point;
+    private Animator weaponAnimator;
+    private Player player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+        weaponAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        weaponAnimator.SetBool("isAtk", player.isAtk);
+        point.SetActive(player.isAtk);
+    }
+}
