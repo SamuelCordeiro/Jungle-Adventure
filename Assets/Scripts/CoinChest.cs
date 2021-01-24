40836fcab596e1c6a982e3ed4ad44adcfa7334f5
Samuel Cordeiro Silva Oliveira <40183730+SamuelCordeiro@users.noreply.github.com>
2021-01-24 01:59:39 -0300

    Coin chest script creation.

---
+++ Assets/Scripts/CoinChest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinChest : MonoBehaviour
+{
+    public int coins;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider) 
+    {
+        if(collider.gameObject.tag == "PointAtk")
+        {
+            GameController.current.AddScore(coins);
+            Destroy(gameObject);
+        }
+    }
+}
