1d07506f0b38bd8ea4abbeb7a50e8e5b17b36f9b
Samuel Cordeiro Silva Oliveira <40183730+SamuelCordeiro@users.noreply.github.com>
2021-01-29 16:05:39 -0300

    Main camera script creation.

---
+++ Assets/Scripts/MainCamera.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamera : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+    private GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(player != null)
+        {
+            Vector3 newPos = new Vector3(player.transform.position.x + 2f, transform.position.y, transform.position.z);
+            transform.position = Vector3.Lerp(transform.position, newPos, speed * Time.deltaTime);
+        }
+    }
+}
