477977fa0e62e59b6724b1caf62e548a59344f1a
Samuel Cordeiro Silva Oliveira <40183730+SamuelCordeiro@users.noreply.github.com>
2021-02-04 16:26:39 -0300

    Enemy point script creation.

---
+++ Assets/Scripts/EnemyPoint.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPoint : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject enemy;
+
+    private void OnTriggerExit2D(Collider2D collider) 
+    {
+        if(collider.gameObject.layer == 8)
+        {
+            switch (enemy.tag)
+            {
+                case "Spider":
+                    gameObject.GetComponentInParent<Spider>().Direction();
+                    break;
+                case "Wolf":
+                    gameObject.GetComponentInParent<Wolf>().Direction();
+                    break;
+                case "Scorpion":
+                    break;
+                default:
+                    break;
+            }
+            //gameObject.GetComponentInParent<Spider>().Direction();
+        }
+    }
+}
