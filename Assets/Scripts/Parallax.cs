0af9a1267402250ea6ce08baae4f60c9aaa6903a
Samuel Cordeiro Silva Oliveira <40183730+SamuelCordeiro@users.noreply.github.com>
2021-02-13 16:53:15 -0300

    Parallax controller creation.

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject mainCamera;
+    [SerializeField]
+    private float speedMovement;
+    private float startPosition;
+    private float lenght;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.position.x;
+        lenght = GetComponent<SpriteRenderer>().bounds.size.x;
+        mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float temp = (mainCamera.transform.position.x * (1 - speedMovement));
+        float distance = (mainCamera.transform.position.x * speedMovement);
+
+        transform.position = new Vector3(startPosition + distance, transform.position.y, transform.position.z);
+
+        if(temp > startPosition + lenght / 2)
+        {
+            startPosition += lenght;
+        }
+        else if(temp < startPosition - lenght / 2)
+        {
+            startPosition -= lenght;
+        }
+    }
+}
