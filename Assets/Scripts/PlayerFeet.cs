f5345e50cc583173a5d18b4e035b418b46804e8b
Samuel Cordeiro Silva Oliveira <40183730+SamuelCordeiro@users.noreply.github.com>
2021-02-15 23:31:14 -0300

    Player movement upgrade.

---
+++ Assets/Scripts/PlayerFeet.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFeet : MonoBehaviour
+{
+    [SerializeField]
+    private bool isGrounded;
+
+    private void OnTriggerStay2D(Collider2D collider) 
+    {
+        if(collider.gameObject.layer == 8)
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collider) 
+    {
+        if(collider.gameObject.layer == 8)
+        {
+            isGrounded = false;
+        }
+        
+    }
+
+    public bool GetIsGrouded()
+    {
+        return isGrounded;
+    }
+}
